bd9606d8a5d22eec7b15bc6ad54c67f03c00ad78
zoltan23 <timothy_tassin@hotmail.com>
2020-07-11 19:01:33 -0600

    deploying to heroku

--- Amazon-review-classifier/website/app.py
+++ Amazon-review-classifier/website/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-    def hello():
-        return "Hello World!"
+@app.route('/hello')
+def helloIndex():
+    return 'Hello World from Python Flask!'
 
 if __name__ == "__main__":
     app.run()
