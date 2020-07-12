35e497dfb26dd01916d38e18898c48daabbf9e0f
zoltan23 <timothy_tassin@hotmail.com>
2020-07-11 18:56:23 -0600

    configuing deployment files

--- Amazon-review-classifier/website/app.py
+++ Amazon-review-classifier/website/app.py
@@ -1,26 +1,37 @@
-import numpy as numpy
-from flask import Flask, request, render_template
-import pickle
-from NLP_main import cv
-from NLP_functions import cleanData
-
-
+from flask import Flask
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    int_features = [x for x in request.form.values()]
-    print(int_features)
-    new_data = cleanData(int_features)
-    new_data = cv.transform(new_data).toarray()
-    prediction = model.predict(new_data)
-    print(prediction)
-    return render_template("index.html", prediction = prediction)
+@app.route("/")
+    def hello():
+        return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run()
+
+
+# import numpy as numpy
+# from flask import Flask, request, render_template
+# import pickle
+# from NLP_main import cv
+# from NLP_functions import cleanData
+
+
+# app = Flask(__name__)
+# model = pickle.load(open('model.pkl', 'rb'))
+
+# @app.route('/')
+# def index():
+#     return render_template("index.html")
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     int_features = [x for x in request.form.values()]
+#     print(int_features)
+#     new_data = cleanData(int_features)
+#     new_data = cv.transform(new_data).toarray()
+#     prediction = model.predict(new_data)
+#     print(prediction)
+#     return render_template("index.html", prediction = prediction)
+
+# if __name__ == "__main__":
+#     app.run(debug = True)
